e01c3b4022e322c8c401629ba79c4ddd981fa0f5
Senadin <senadin.botic@gmail.com>
2019-05-08 00:02:45 +0200

    Improve bahaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
 jQuery(function ($) {
     $(document).ready(function () {
-    
+
         // Menu
         $('#navtoggle').on('click', function () {
             //$('.nav').toggleClass('open');
             $('body').removeClass('search-open');
             $('body').toggleClass('menu-open');
         });
-    
+
         // Mobile Search Open
         $('.mobile-search-icon').on('click', function () {
             $('body').toggleClass('search-open');
             $('.nav').removeClass('open');
             $('body').removeClass('menu-open');
         });
-        $('.site-content').on('click', function(e) {
+        $('.site-content').on('click', function (e) {
             console.log('test');
             if ($(e.target).is('.header-search') === false) {
-              $('body').removeClass('search-open');
-              
+                $('body').removeClass('search-open');
+
             }
         });
-        
+
         // Open/Close Desktop Search Open
         $('.search-icon-wrapper').on('click', function () {
             $('.header-search').slideDown();
@@ -29,7 +29,7 @@ jQuery(function ($) {
         $('.desktop-close-search').on('click', function () {
             $('.header-search').slideUp();
         });
-    
+
         // Hide Sign Up Form
         $('.close-promo-offers').on('click', function () {
             $(this).parents('.promo-offers-container').slideUp();
@@ -38,18 +38,18 @@ jQuery(function ($) {
                 $('body').removeClass('has-promo-offers');
             });
         });
-    
+
         // Check Mobile OS
-        if (navigator.userAgent.match(/Android/i)) {
-            $('.download-app').addClass('android-app');
-        }
-        else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-            $('.download-app').addClass('ios-app');
-        }
-        else {
-            $('.download-app').addClass('desktop-app');
-        }
-    
+        // if (navigator.userAgent.match(/Android/i)) {
+        //     $('.download-app').addClass('android-app');
+        // }
+        // else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+        //     $('.download-app').addClass('ios-app');
+        // }
+        // else {
+        //     $('.download-app').addClass('desktop-app');
+        // }
+
         // Check Subscribe Form
         if ($('.promo-offers-container').length) {
             $('body').addClass('has-promo-offers');
@@ -66,6 +66,6 @@ jQuery(function ($) {
         else {
             $('body').removeClass('has-promo-offers');
         }
-    
+
     });
-    });
+});
